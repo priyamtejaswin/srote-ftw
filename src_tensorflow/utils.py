e9a02be8200192a78dbd4a01ae25260f2831f87d
priyamtejaswin <priyamtejaswin@gmail.com>
2019-04-26 23:06:43 +0530

    `load_fnames` added.
     Changes to be committed:
    	modified:   src_tensorflow/utils.py

--- src_tensorflow/utils.py
+++ src_tensorflow/utils.py
@@ -5,3 +5,47 @@ created by: Priyam Tejaswin (tejaswin.p@flipkart.com)
 
 Utility functions. Data, io, processing, etc.
 """
+
+import os
+import sys
+
+
+VFDEL = '___' ## VideoName___FrameId delimiter.
+NFRAMES = 2 ## Number of frames considered per sample.
+
+
+def _fskey(f):
+    vname, fid = f.rstrip('.png').split(VFDEL)
+    fid = int(fid)
+    return vname, fid
+
+def load_fnames(fdir):
+    all_frames = []
+
+    vdirs = [os.path.join(fdir, d) for d in os.listdir(fdir)]
+    vdirs = [d for d in vdirs if os.path.isdir(d)]
+    print "Found %d vid files."%len(vdirs)
+
+    for dpath in vdirs:
+        print "In %s"%dpath
+        frames = [f for f in os.listdir(dpath) if f.endswith('.png')]
+        print "\tFound %d frames."%len(frames)
+
+        frames = sorted(frames, key=lambda x:_fskey(x))
+        frames = [os.path.join(dpath, f) for f in frames]
+
+        ## Group sorted frames by NFRAMES.
+        motion_frames = []
+        for i in range(len(frames) - NFRAMES + 1):
+            motion_frames.append(tuple(frames[i : i+NFRAMES]))
+
+        ## Add to master list.
+        all_frames.extend(motion_frames)
+
+    print "Found %d frame groups of length %d."%(len(all_frames), NFRAMES)
+    return all_frames
+
+
+if __name__ == '__main__':
+    dirpath = sys.argv[1]
+    load_fnames(dirpath)
