488ce62a497f61412cdbd26155fe1aa5b9d2af40
priyamtejaswin <priyamtejaswin@gmail.com>
2019-04-28 23:39:37 +0530

    `HACK` for the fusion op. This might be good enough though...
     Changes to be committed:
    	modified:   src_tensorflow/fusion.py

--- src_tensorflow/fusion.py
+++ src_tensorflow/fusion.py
@@ -9,14 +9,16 @@ Refer to Fig.4 for comparision of architectures.
 """
 
 
+import numpy as np
 import tensorflow as tf
 keras = tf.keras
 from keras.layers import TimeDistributed
 from keras.layers import Conv2D
 from keras.activations import  relu
+from keras.layers import Layer
 
 
-def EarlyFusion(timeframes, iheight, iwidth, channels=3,
+def early_fusion(timeframes, iheight, iwidth, channels=3,
                 nfilters=None, ksize=3, activation=relu):
     """
     A wrapper over TimeDistributed layer for Early Fusion.
@@ -40,7 +42,45 @@ def EarlyFusion(timeframes, iheight, iwidth, channels=3,
     )
 
 
+class EarlyFusion(Layer):
+    """
+    A wrapper over TimeDistributed layer for Early Fusion.
+    Returns a TimeDistributed Conv2d layer you can add to the model.
+    """
+    def __init__(self, timeframes, iheight, iwidth, channels=3,
+                nfilters=None, ksize=3, activation=relu):
+        """
+        :param timeframes: Number of timesteps/frames; the first index AFTER batch.
+        :param iheight: Height of frame.
+        :param iwidth: Width of frame.
+        :param channels: Image channels.
+        :param nfilters: Filters for Conv2d; will be imputed if None.
+        :param ksize: Kernel size for Conv2d.
+        :param activation: Activation for Conv2d.
+        :return: A TimeDistributed Conv2d layer you can add to the model.
+        """
+        super(EarlyFusion, self).__init__()
+
+        self.convTd = TimeDistributed(
+            Conv2D(nfilters, ksize, activation=activation),
+            input_shape=(timeframes, iheight, iwidth, channels)
+        )
+
+    def call(self, inputs):
+        x = self.convTd(inputs)
+        x = tf.reduce_sum(x, axis=1)
+        return x
+
+
 if __name__ == '__main__':
-    print EarlyFusion(
-        timeframes=3, iheight=32, iwidth=32, channels=3
-    )
+    tf.enable_eager_execution()
+    print "TF Executing Eagerly?", tf.executing_eagerly()
+
+    x = tf.constant(np.ones((5, 3, 32, 32, 3)))
+    print 'x', x.shape
+
+    y = EarlyFusion(
+        timeframes=3, iheight=32, iwidth=32, channels=3, nfilters=12
+    )(x)
+
+    print 'y', y.shape
