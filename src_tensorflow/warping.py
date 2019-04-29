e1d8f01f54272a2209ebb389bf5eb6c44bee45f3
Akshay Chawla <chawla.akshay1234@gmail.com>
2019-04-29 00:52:22 -0400

    Spatial motion compensation is in a working state
    1. warping.py: use motion_compensation(reference_frame, other_frame),
    pass the 2 frames and it will internally call coarse_flow and fine_flow
    and warp the other_frame towards the reference_frame and return the
    result. Check test_flow() for an example of how to do this with numpy
    array as example with eager mode on.

--- src_tensorflow/warping.py
+++ src_tensorflow/warping.py
@@ -4,8 +4,6 @@ import tensorflow as tf
 from fusion import EarlyFusion
 from tensorflow.keras import layers
 
-tf.enable_eager_execution()
-
 
 def coarse_flow(frames): 
 
@@ -19,73 +17,61 @@ def coarse_flow(frames):
     # subpixel conv upscaling x4 
     out = tf.depth_to_space(out, 4)
 
-    return ef,out 
+    return out 
 
-def fine_flow(early_fused, coarse_flow_vectors, coarse_compensated_frame): 
+def fine_flow(frames, coarse_flow_vectors, coarse_compensated_frame): 
 
-    fine_input = tf.concat((early_fused, coarse_flow_vectors, coarse_compensated_frame), axis=-1)
-    out = layers.Conv2D(filters=24, kernel_size=(5,5), strides=(2,2), activation="relu", padding="same")(fine_input)
+    ef = EarlyFusion(timeframes=2, iheight=32, iwidth=32, channels=3)(frames)
+    out = tf.concat((ef, coarse_flow_vectors, coarse_compensated_frame), axis=-1)
+    out = layers.Conv2D(filters=24, kernel_size=(5,5), strides=(2,2), activation="relu", padding="same")(out)
     out = layers.Conv2D(filters=24, kernel_size=(3,3), strides=(1,1), activation="relu", padding="same")(out)
     out = layers.Conv2D(filters=24, kernel_size=(3,3), strides=(1,1), activation="relu", padding="same")(out)
     out = layers.Conv2D(filters=24, kernel_size=(3,3), strides=(1,1), activation="relu", padding="same")(out)
     out = layers.Conv2D(filters=8, kernel_size=(3,3), strides=(1,1), activation="tanh", padding="same")(out)
+
     # subpixel conv upscaling x2 
     out = tf.depth_to_space(out, 2)
 
     return out
 
-def test_flow():
-
-    import ipdb; ipdb.set_trace()
-    frames = np.random.randn(1,2,32,32,3) # batch x (I_t, I_t+1) x height x width x channels(RGB)
-    frames = frames.astype(np.float32)
+def motion_compensation(reference_frame, other_frame): 
+    """
+    use spatial transformer networks to compensate other_frame to reference_frame 
+    reference_frame: batch x 32 x 32 x 3 
+    other_frame: batch x 32 x 32 x 3 
+    """ 
+    frames = tf.stack((reference_frame, other_frame), axis=1)
 
     # coarse flow
-    early_fused, coarse_flow_vectors = coarse_flow(frames)
+    coarse_flow_vectors = coarse_flow(frames)
 
     # warp 
-    next_frame = frames[:,1,:,:,:]
-    coarse_compensated_frame = tf.contrib.image.dense_image_warp(next_frame, coarse_flow_vectors)
+    coarse_compensated_frame = tf.contrib.image.dense_image_warp(other_frame, coarse_flow_vectors)
 
     # fine flow
-    fine_flow_vectors = fine_flow(early_fused, coarse_flow_vectors, coarse_compensated_frame)
+    fine_flow_vectors = fine_flow(frames, coarse_flow_vectors, coarse_compensated_frame)
 
     # combine coarse_flow_vectors and fine_flow_vectors to get the final total_flow_vectors
     total_flow = coarse_flow_vectors + fine_flow_vectors
 
-    # use total_flow to compensate i_t+1 back to i_t 
-    compensated_frame = tf.contrib.image.dense_image_warp(next_frame, total_flow)
+    # warp  
+    compensated_frame = tf.contrib.image.dense_image_warp(other_frame, total_flow)
+
+    return compensated_frame
+    
+
+
+def test_flow():
+
+    import ipdb; ipdb.set_trace()
+    reference_frame = np.random.randn(1,32,32,3).astype(np.float32)
+    other_frame = np.random.randn(1,32,32,3).astype(np.float32)
+
+    compensated_frame = motion_compensation(reference_frame, other_frame)
 
     print(compensated_frame.shape)
 
 
-
-def test_warping():
-    x = np.random.randn(4,4)
-    y = np.zeros_like(x) 
-    y[:, 1:] += x[:, :3]
-    flow = np.zeros(shape=(1,4,4,2))
-    flow[0,:,:,1] += 1
-    x = np.expand_dims(x, axis=0)
-    x = np.expand_dims(x, axis=3)
-    y = np.expand_dims(y, axis=0)
-    y = np.expand_dims(y, axis=3)
-
-    print(x.shape)
-    print(y.shape)
-
-    _y = tf.contrib.image.dense_image_warp(x, flow)
-    _y = _y.numpy()
-
-    print(_y.shape)
-
-    # checking if my intution for warping is correct 
-    same = np.allclose(_y[0,:,1:,0], y[0,:,1:,0])
-    print("Intuition correct? {}".format(same))
-
-    import ipdb; ipdb.set_trace()
-
-    print("..done")
-
 if __name__ == "__main__":
+    tf.enable_eager_execution()
     test_flow()
