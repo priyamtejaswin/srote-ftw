a1e58b770f6502cf8a1de11fefa4dde73673490b
Akshay Chawla <chawla.akshay1234@gmail.com>
2019-04-28 23:41:20 -0400

    Coarse and Fine flow warping works
    1. warping.py : check out test_flow() which gives an example on how to
    use the coarse_flow() and fine_flow() functions to perform spatial
    transformer motion compensation to get the final motion compensated
    frame.

--- src_tensorflow/warping.py
+++ src_tensorflow/warping.py
@@ -9,8 +9,8 @@ tf.enable_eager_execution()
 
 def coarse_flow(frames): 
 
-    out = EarlyFusion(timeframes=2, iheight=32, iwidth=32, channels=3)(frames)
-    out = layers.Conv2D(filters=24, kernel_size=(5,5), strides=(2,2), activation="relu", padding="same")(out)
+    ef = EarlyFusion(timeframes=2, iheight=32, iwidth=32, channels=3)(frames)
+    out = layers.Conv2D(filters=24, kernel_size=(5,5), strides=(2,2), activation="relu", padding="same")(ef)
     out = layers.Conv2D(filters=24, kernel_size=(3,3), strides=(1,1), activation="relu", padding="same")(out)
     out = layers.Conv2D(filters=24, kernel_size=(5,5), strides=(2,2), activation="relu", padding="same")(out)
     out = layers.Conv2D(filters=24, kernel_size=(3,3), strides=(1,1), activation="relu", padding="same")(out)
@@ -19,17 +19,44 @@ def coarse_flow(frames):
     # subpixel conv upscaling x4 
     out = tf.depth_to_space(out, 4)
 
-    return out 
+    return ef,out 
+
+def fine_flow(early_fused, coarse_flow_vectors, coarse_compensated_frame): 
+
+    fine_input = tf.concat((early_fused, coarse_flow_vectors, coarse_compensated_frame), axis=-1)
+    out = layers.Conv2D(filters=24, kernel_size=(5,5), strides=(2,2), activation="relu", padding="same")(fine_input)
+    out = layers.Conv2D(filters=24, kernel_size=(3,3), strides=(1,1), activation="relu", padding="same")(out)
+    out = layers.Conv2D(filters=24, kernel_size=(3,3), strides=(1,1), activation="relu", padding="same")(out)
+    out = layers.Conv2D(filters=24, kernel_size=(3,3), strides=(1,1), activation="relu", padding="same")(out)
+    out = layers.Conv2D(filters=8, kernel_size=(3,3), strides=(1,1), activation="tanh", padding="same")(out)
+    # subpixel conv upscaling x2 
+    out = tf.depth_to_space(out, 2)
+
+    return out
 
 def test_flow():
 
+    import ipdb; ipdb.set_trace()
     frames = np.random.randn(1,2,32,32,3) # batch x (I_t, I_t+1) x height x width x channels(RGB)
     frames = frames.astype(np.float32)
 
-    coarse_flow_vectors = coarse_flow(frames)
-    print(coarse_flow_vectors.shape)
-    import ipdb; ipdb.set_trace()
+    # coarse flow
+    early_fused, coarse_flow_vectors = coarse_flow(frames)
 
+    # warp 
+    next_frame = frames[:,1,:,:,:]
+    coarse_compensated_frame = tf.contrib.image.dense_image_warp(next_frame, coarse_flow_vectors)
+
+    # fine flow
+    fine_flow_vectors = fine_flow(early_fused, coarse_flow_vectors, coarse_compensated_frame)
+
+    # combine coarse_flow_vectors and fine_flow_vectors to get the final total_flow_vectors
+    total_flow = coarse_flow_vectors + fine_flow_vectors
+
+    # use total_flow to compensate i_t+1 back to i_t 
+    compensated_frame = tf.contrib.image.dense_image_warp(next_frame, total_flow)
+
+    print(compensated_frame.shape)
 
 
 
@@ -61,4 +88,4 @@ def test_warping():
     print("..done")
 
 if __name__ == "__main__":
-    coarse_flow() 
+    test_flow()
