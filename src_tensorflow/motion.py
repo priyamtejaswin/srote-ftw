92d1aad1c03ce3e5171b36fee6cb7a40b758d147
priyamtejaswin <priyamtejaswin@gmail.com>
2019-05-01 22:21:52 +0530

    Layers for flow and motion.
     Changes to be committed:
    	new file:   motion.py

---
+++ src_tensorflow/motion.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""
+created at: 01/05/19 10:20 PM
+created by: Priyam Tejaswin (tejaswin.p@flipkart.com)
+
+Custom layers for flow-estimation and motion-compensation.
+Refer `src_tensorflow/warping.py`
+"""
