e018ceabb76bcb21cfe0f8b2555b2cefb4f9ce59
priyamtejaswin <priyamtejaswin@gmail.com>
2019-05-07 11:33:11 +0530

    Training file.
     Changes to be committed:
    	new file:   train.py

---
+++ src_tensorflow/train.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""
+created at: 2019-05-07 11:32
+created by: Priyam Tejaswin (tejaswin.p@flipkart.com), Akshay Chawla (akshaych@andrew.cmu.edu)
+
+Main training file.
+
+"""
